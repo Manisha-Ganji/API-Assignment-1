0ff189fc0b3b02e150dc8b70fff25b05c304a12e
Group27Devops <2023mt03130@wilp.bits-pilani.ac.in>
2024-10-17 11:12:09 +0530

    Code refactoring

--- EdaAndPreprocessingTasks/Correlationcoefficient.py
+++ EdaAndPreprocessingTasks/Correlationcoefficient.py
@@ -1,8 +1,12 @@
+
 from tkinter import TRUE
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import io
+import base64
+
  
 df = pd.read_csv("../Datasets/cardiovascular_dataset.csv")
 
@@ -16,4 +20,18 @@ cor = df.corr()
 # Plotting Heatmap
 plt.figure(figsize = (12,6))
 sns.heatmap(cor, annot=True)
-plt.show()
+
+
+# Save the heatmap to a buffer
+buf = io.BytesIO()
+plt.savefig(buf, format='png')
+buf.seek(0)
+
+# Encode the image in base64 and log it
+img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+
+# Save the heatmap as a file
+plt.savefig("../output/heat_map.png")
+
+# Close the buffer
+buf.close()
--- EdaAndPreprocessingTasks/PearsonCorrelation.py
+++ EdaAndPreprocessingTasks/PearsonCorrelation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from scipy.stats import pearsonr
 from matplotlib import pyplot as plt
+import io
+import base64
 
 # Import your data into Python
 df = pd.read_csv("../Datasets/cardiovascular_dataset.csv")
@@ -20,7 +22,22 @@ print('Pearson correlation: %.3f' % corr)
 
 # Draw a Plot of the relationship
 # 'Age' on the X Axis and 'Cardio' on the Y axis
-from matplotlib import pyplot
-pyplot.scatter(list1, list2)
-pyplot.title('Scatter plot of Age vs Chance of CardioVascular disease')
-pyplot.show()
+
+plt.scatter(list1, list2)
+plt.xlabel('Age')
+plt.ylabel('Chance of CardioVascular disease')
+plt.title('Scatter plot of Age vs Chance of CardioVascular disease')
+
+# Save the plot to a buffer
+buf = io.BytesIO()
+plt.savefig(buf, format='png')
+buf.seek(0)
+
+# Encode the image in base64 and log it
+img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+
+# Save the plot as a file
+plt.savefig("../output/scatter_plot.png")
+
+# Close the buffer
+buf.close()
