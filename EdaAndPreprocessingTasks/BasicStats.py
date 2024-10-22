65756de68e2e71ea36724add522d5026c5f01f1a
Group27Devops <2023mt03130@wilp.bits-pilani.ac.in>
2024-10-22 12:45:02 +0530

    added comments

--- EdaAndPreprocessingTasks/BasicStats.py
+++ EdaAndPreprocessingTasks/BasicStats.py
@@ -5,31 +5,35 @@ import seaborn as sns
 
 # Load the dataset
 df = pd.read_csv("../Datasets/health_data.csv")
+print("\nDisplaying the original daatset after loading into dataframe:\n")
 print(df)
 # Summary statistics
-print("\nSummary Statistics:")
+print("\nSummary Statistics:\n")
 print(df.describe(include='all'))
 
 # Checking for missing values
-print("\nMissing Values:")
+print("\nMissing Values:\n")
 print(df.isnull().sum())
 
 # Data type information
-print("\nData Types:")
+print("\nData Types:\n")
 print(df.dtypes)
 
-#Converting data[age]
+# Converting data[age]
+print("\nConverting age from number days to number of years\n")
 df['age'] = (df['age'] / 365.25).astype(int)
 print(df)
 df.to_csv("../Datasets/cardiovascular_dataset.csv")
 
-#View all duplicate rows
+# View all duplicate rows
 duplicates = df.duplicated()
 duplicate_rows = df[duplicates]
 
 # Print or inspect duplicate rows
+print("\nDuplicate rows\n")
 print(duplicate_rows)
-#removes the id column from new csv file and saves it.
+# removes the id column from new csv file and saves it.
+print("\nRemoving patient id column\n")
 df = df.drop(df.columns[0], axis=1)
 print(df)
 df.to_csv("../Datasets/cardiovascular_dataset.csv")
--- EdaAndPreprocessingTasks/Binning.py
+++ EdaAndPreprocessingTasks/Binning.py
@@ -2,23 +2,25 @@ import pandas as pd
 import numpy as np
  
 df = pd.read_csv("../Datasets/cardiovascular_dataset.csv")
-print(df)
+#print(df)
 
 # 1. Distance binning
 # Formula -> interval = (max-min) / Number of Bins
 # Let us consider the 'Age' continuous value column for binning
 min_value = df['age'].min()
 max_value = df['age'].max()
-print(min_value)
-print(max_value)
+print("\nMinimum age: " +str(min_value))
+print("\nMaximum age: " +str(max_value))
 
 # Suppose the bin size is 5
 # linspace returns evenly spaced numbers over a specified interval. 
 # Returns num evenly spaced samples, calculated over the interval [start, stop].
 bins = np.linspace(min_value,max_value,5)
+print("\nSize of bin: 5")
+print("\n Display categrories of age based on bin size")
 print(bins)
 
-labels = ['Juvenile', 'Adult', 'Middle Age', 'Senior Citizen'];
+labels = ['Juvenile', 'Adult', 'Middle Age', 'Senior Citizen']
 
 # We can use the cut() function to convert the numeric values of the column Age into the categorical values.
 # We need to specify the bins and the labels.
