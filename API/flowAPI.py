58adf7166e835a970f350c09f6676826056dd959
Group27Devops <2023mt03130@wilp.bits-pilani.ac.in>
2024-10-22 17:01:10 +0530

    updated API logics

--- API/flowAPI.py
+++ API/flowAPI.py
@@ -6,13 +6,13 @@ import requests
 import json
 
 # Replace these variables with your actual Prefect Cloud credentials
-PREFECT_API_KEY = "pnu_M6BY8iNMSIdRgRrO0jWdit0hHcYNuP2dzRLW"  # Your Prefect Cloud API key
+PREFECT_API_KEY = "pnu_YtZWDV5docvM1I1XpQysa3EH1EKkqM4CtdAW"  # Your Prefect Cloud API key
 ACCOUNT_ID = "1d41fc5f-7f44-4380-ac43-4ea4ccce4bf0"  # Your Prefect Cloud Account ID
 WORKSPACE_ID = "f47716bb-0023-4eed-b03b-6131b4675dce"  # Your Prefect Cloud Workspace ID
 FLOW_ID = "a8a07622-f4d1-4e01-9b1c-31e614f58eec"  # Your Flow ID
 
 # Correct API URL to get flow details
-PREFECT_API_URL = f"https://app.prefect.cloud/account/{ACCOUNT_ID}/workspace/{WORKSPACE_ID}/flows/flow/{FLOW_ID}"
+PREFECT_API_URL = f"https://api.prefect.cloud/api/accounts/{ACCOUNT_ID}/workspaces/{WORKSPACE_ID}/flows/{FLOW_ID}"
 print(PREFECT_API_URL)
 
 # Set up headers with Authorization
@@ -23,8 +23,13 @@ response = requests.get(PREFECT_API_URL, headers=headers)
 
 # Check the response status
 if response.status_code == 200:
-    #flow_info = json.dumps(response)
-    print(response)
+    flow_info = response.json()
+    print("\n\n")
+    print(flow_info)
+    print(f"\n\nFlow id : {flow_info['id']}")
+    print(f"\n\nFlow name : {flow_info['name']}")
+    print(f"\n\nFlow is created on : {flow_info['created']}")
+    print(f"\n\nFlow is updated on : {flow_info['updated']}")
 else:
     print(f"Error: Received status code {response.status_code}")
     print(f"Response content: {response.text}")
--- API/runAPI.py
+++ API/runAPI.py
@@ -9,12 +9,12 @@ import json
 PREFECT_API_KEY = "pnu_YtZWDV5docvM1I1XpQysa3EH1EKkqM4CtdAW"  # Your Prefect Cloud API key
 ACCOUNT_ID = "1d41fc5f-7f44-4380-ac43-4ea4ccce4bf0"  # Your Prefect Cloud Account ID
 WORKSPACE_ID = "f47716bb-0023-4eed-b03b-6131b4675dce"  # Your Prefect Cloud Workspace ID
-DEPLOYMENT_ID = "37a06923-4096-4fff-b44e-e343fb241c94"  # Your Deployment ID
+RUN_ID = "41bdb73c-eeca-4514-ba68-bbdc4f0159c2"  # Your run ID
 
-# Correct API URL to get deployment details
-PREFECT_API_URL = f"https://api.prefect.cloud/api/accounts/{ACCOUNT_ID}/workspaces/{WORKSPACE_ID}/deployments/{DEPLOYMENT_ID}"
-
+# Correct API URL to get flow details
+PREFECT_API_URL = f"https://api.prefect.cloud/api/accounts/{ACCOUNT_ID}/workspaces/{WORKSPACE_ID}/flow_runs/{RUN_ID}"
 print(PREFECT_API_URL)
+
 # Set up headers with Authorization
 headers = {"Authorization": f"Bearer {PREFECT_API_KEY}"}
 
@@ -23,14 +23,14 @@ response = requests.get(PREFECT_API_URL, headers=headers)
 
 # Check the response status
 if response.status_code == 200:
-    deployment_info = response.json()
+    flow_info = response.json()
     print("\n\n")
-    print(deployment_info)
-    print(f"\n\nDeployment id : {deployment_info['id']}")
-    print(f"\n\nDeployment name : {deployment_info['name']}")
-    print(f"\n\nDeployment created on : {deployment_info['created']}")
-    print(f"\n\nDeployment is scheduled for : {(deployment_info['schedule'])['interval']} seconds")
-    print(f"\n\nDeployment is scheduled as per : {(deployment_info['schedule'])['timezone']} timezone")
+    print(flow_info)
+    print(f"\n\nFlow run id : {flow_info['id']}")
+    print(f"\n\nFlow run name : {flow_info['name']}")
+    print(f"\n\nFlow run is created on : {flow_info['created']}")
+    print(f"\n\nAsscociated Deployment id : {flow_info['deployment_id']}")
+    print(f"\n\nAsscociated Flow id : {flow_info['flow_id']}")
 else:
     print(f"Error: Received status code {response.status_code}")
     print(f"Response content: {response.text}")
